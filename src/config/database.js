cbcb0b7817b1605e400282f5b1633f3248f34350
Chanthima Tosungneon <chanthima.t@astartechs.com>
2024-08-30 14:00:22 +0700

    db4

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,15 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 // Initialize Sequelize with environment variables
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
+  process.env.POSTGRES_DATABASE,   // Database name
+  process.env.POSTGRES_USER,       // Username
+  process.env.POSTGRES_PASSWORD,   // Password
   {
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    port: process.env.POSTGRES_PORT,
-    ssl: process.env.USE_SSL === "true" ? { rejectUnauthorized: false } : false,
-    logging: false, // Set to `true` if you need to debug queries
+    host: process.env.POSTGRES_HOST,  // Host
+    port: process.env.POSTGRES_PORT,  // Port (optional)
+    dialect: "postgres",  
+    logging: false             
   }
 );
 
@@ -19,11 +17,11 @@ async function initializeDatabase() {
   try {
     // Authenticate the connection
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    // console.log("Connection has been established successfully.");
 
     // Sync all models
     await sequelize.sync({ alter: true }); // Use `alter: true` to update the tables without dropping them
-    console.log("All models were synchronized successfully.");
+    // console.log("All models were synchronized successfully.");
   } catch (error) {
     // Log any connection errors
     console.error("Unable to connect to the database:", error);
